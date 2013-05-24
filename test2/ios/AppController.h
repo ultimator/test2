071fb583463007d1477bd11b94c7c906dc6affa2
ultimator <dill_yu@163.com>
2013-05-24 14:34:43 +0800

    Initial Commit

---
+++ test2/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  test2AppController.h
+//  test2
+//
+//  Created by MagicStudio on 13-5-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
